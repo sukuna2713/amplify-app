d611e89e42c6b06e5b22844827f08171c64f9ccb
sukuna2713 <sukuna2713@gmail.com>
2022-01-19 23:36:17 +0900

    add authentication

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import awsExports from './aws-exports'
+import Amplify from 'aws-amplify';
+Amplify.configure(awsExports)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
